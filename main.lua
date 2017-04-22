93fde07d7cf552a48e0c289639d29c1066bdb315
Peter Rust <peter@cornerstonenw.com>
2017-04-22 07:55:18 -0700

    Added game camera & implemented auto-scrolling

--- main.lua
+++ main.lua
@@ -1,32 +1,66 @@
 local anim8 = require('anim8')
 local sti = require('sti')
+local gamera = require("gamera")
+-- require('mobdebug').start()
+
 local ver = '0.0.1'
 local width, height
-
--- draw everything to a canvas so we can do linear/nearest-neightbor rescaling
--- https://love2d.org/forums/viewtopic.php?t=7911#p49010
-local canvas
+local camera
+local map
+local auto_scroll_region = 0.12 -- 12% of the width/height on all sides of window
+local auto_scroll_speed = 500
 
 function love.load()
+  love.graphics.setDefaultFilter( 'nearest', 'nearest' )
   love.window.setTitle('One Golden Sun ' .. ver)
   width, height = love.graphics.getDimensions()
   love.window.setMode(width, height, {resizable=true, vsync=false, minwidth=400, minheight=300})
-  canvas = love.graphics.newCanvas(width / 2, height / 2)
-  canvas:setFilter("nearest", "nearest")
+  camera = gamera.new(0, 0, 2000, 2000) -- TODO: pull this from the map?
+  camera:setScale(2.0)
 
   map = sti("world.lua", {"bump"})
 end
 
+function love.update(dt)
+  map:update(dt)
+  mouse_x, mouse_y = love.mouse.getPosition()
+
+  -- auto-scroll if mouse is near window edges (and still in the window)
+  if mouse_x > 0 and mouse_x < width and mouse_y > 0 and mouse_y < width then
+    local a_s_region_horiz = width * auto_scroll_region
+    local a_s_region_vert = height * auto_scroll_region
+    local cam_x, cam_y = camera:getPosition()
+    
+    local cam_x_pct = 0
+    if mouse_x < a_s_region_horiz then
+      cam_x_pct = -(a_s_region_horiz - mouse_x) / a_s_region_horiz
+    elseif mouse_x > width - a_s_region_horiz then
+      cam_x_pct = (mouse_x - (width - a_s_region_horiz)) / a_s_region_horiz
+    end
+    cam_x = cam_x + cam_x_pct * auto_scroll_speed * dt
+
+    local cam_y_pct = 0
+    if mouse_y < a_s_region_vert then
+      cam_y_pct = -(a_s_region_vert - mouse_y) / a_s_region_vert
+    elseif mouse_y > height - a_s_region_vert then
+      cam_y_pct = (mouse_y - (height - a_s_region_vert)) / a_s_region_vert
+    end
+    cam_y = cam_y + cam_y_pct * auto_scroll_speed * dt
+    
+    camera:setPosition(cam_x, cam_y)
+  end
+end
+
+
 function love.draw()
-  love.graphics.setCanvas(canvas) --This sets the draw target to the canvas
-  map:draw()
-  love.graphics.setCanvas() --This sets the target back to the screen
-  love.graphics.draw(canvas, 0, 0, 0, 2, 2)
+  camera:draw(function(l, t, w, h)
+    map:draw()
+  end)
 end
 
 function love.resize(w, h)
   width = w
   height = h
-  map:resize(w, h)
-  canvas = love.graphics.newCanvas(width / 2, height / 2)
+  map:resize(width, height)
+  camera:setWindow(0, 0, width, height)
 end
